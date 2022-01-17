848e915aee51f1cb62204faa12ca65da05651783
Steve Jones <jonesst2608@gmail.com>
2022-01-17 18:09:56 +0000

    Getting charts to work

--- pages/dashboard/sales_chart.py
+++ pages/dashboard/sales_chart.py
@@ -1,4 +1,18 @@
 from dash import html
+from dash_chartist import DashChartist
+
+data = {
+    "labels": ['W1', 'W2', 'W3', 'W4', 'W5', 'W6', 'W7', 'W8', 'W9', 'W10'],
+    "series": [ [1, 2, 4, 8, 6, -2, -1, -4, -6, -2] ]
+}
+
+options = {
+    "high": 10,
+    "low": -10,
+}
+
+chartType = 'Bar'
+
 
 def salesChart():
     return  html.Div([
@@ -19,7 +33,7 @@ def salesChart():
                 ], className='d-flex ms-auto')
             ], className='card-header d-sm-flex flex-row align-items-center flex-0'),
             html.Div([
-                html.Div(className='ct-chart-sales-value ct-double-octave ct-series-g')
+                DashChartist(className='ct-chart-sales-value ct-double-octave', type=chartType, options=options, data=data)
             ], className='card-body p-2')
         ], className='card bg-yellow-100 border-0 shadow')
     ], className='col-12 mb-4')
