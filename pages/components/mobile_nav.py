d55a087d579287fc4332a2f34882851facf1cfc5
Steve Jones <jonesst2608@gmail.com>
2022-01-18 17:38:52 +0000

    Tidy up

--- pages/components/mobile_nav.py
+++ pages/components/mobile_nav.py
@@ -25,7 +25,9 @@ def mobileNavBar():
         ], className='d-flex align-items-center')
     ], className='navbar navbar-dark navbar-theme-primary px-4 col-12 d-lg-none')
 
+
 def mobileSidebarHeader():
+    """ Mobile only sidebar header"""
     return html.Div([
         html.Div([
             # Snip avatar
@@ -38,14 +40,12 @@ def mobileSidebarHeader():
             ], className='d-block')
         ], className='d-flex align-items-center'),
 
-        # Collapse cross X
+        # Sidebar close [X] icon
 
         html.Div([
             html.A([
-                Svg([
-                    Path(fillRule='evenodd', d='M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z', clipRule='evenodd')
-                ], className='icon icon-xs', fill='currentColor', viewBox='0 0 20 20', xmlns='http://www.w3.org/2000/svg')
+                crossIcon(),
             ], href='#sidebarMenu', **{"data-bs-toggle": "collapse", "data-bs-target": "#sidebarMenu", "aria-controls": "sidebarMenu", "aria-expanded": "true", "aria-label": "Toggle navigation"})
         ], className='collapse-close d-md-none')
-    ], className='user-card d-flex d-md-none align-items-center justify-content-between justify-content-md-center pb-4')
 
+    ], className='user-card d-flex d-md-none align-items-center justify-content-between justify-content-md-center pb-4')
