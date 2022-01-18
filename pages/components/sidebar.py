cf7190db365d580f802029e073f1133ddd058c2d
Steve Jones <jonesst2608@gmail.com>
2022-01-18 10:10:21 +0000

    Rationalising the sidebar

--- pages/components/sidebar.py
+++ pages/components/sidebar.py
@@ -31,6 +31,22 @@ def arrowIcon():
         Path(fillRule='evenodd', d='M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z', clipRule='evenodd')
     ], className='icon icon-sm', fill='currentColor', viewBox='0 0 20 20', xmlns='http://www.w3.org/2000/svg')
 
+def fireIcon():
+    return Svg([
+        Path(fillRule='evenodd', d='M12.395 2.553a1 1 0 00-1.45-.385c-.345.23-.614.558-.822.88-.214.33-.403.713-.57 1.116-.334.804-.614 1.768-.84 2.734a31.365 31.365 0 00-.613 3.58 2.64 2.64 0 01-.945-1.067c-.328-.68-.398-1.534-.398-2.654A1 1 0 005.05 6.05 6.981 6.981 0 003 11a7 7 0 1011.95-4.95c-.592-.591-.98-.985-1.348-1.467-.363-.476-.724-1.063-1.207-2.03zM12.12 15.12A3 3 0 017 13s.879.5 2.5.5c0-1 .5-4 1.25-4.5.5 1 .786 1.293 1.371 1.879A2.99 2.99 0 0113 13a2.99 2.99 0 01-.879 2.121z', clipRule='evenodd')
+    ], className='icon icon-xs me-2', fill='currentColor', viewBox='0 0 20 20', xmlns='http://www.w3.org/2000/svg')
+
+def crossIcon():
+    return  Svg([
+        Path(fillRule='evenodd', d='M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z', clipRule='evenodd')
+    ], className='icon icon-xs', fill='currentColor', viewBox='0 0 20 20', xmlns='http://www.w3.org/2000/svg')
+
+
+def signoutIcon():
+    return  Svg([
+        Path(strokeLinecap='round', strokeLinejoin='round', strokeWidth='2', d='M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1')
+    ], className='icon icon-xxs me-1', fill='none', stroke='currentColor', viewBox='0 0 24 24', xmlns='http://www.w3.org/2000/svg'),
+
 
 def sidebarLink(text, icon, href, active=""):
     return  html.Li([
@@ -40,6 +56,17 @@ def sidebarLink(text, icon, href, active=""):
             ], className='sidebar-icon'),
             html.Span(text, className='mt-1 ms-1 sidebar-text')
         ], href=href, className='nav-link d-flex align-items-center')
+    ], className=f'nav-item {active}')
+
+
+def sidebarButtonLink(text, icon, href, active=""):
+    return  html.Li([
+        html.A([
+            html.Span([
+                icon()
+            ], className='sidebar-icon d-inline-flex align-items-center justify-content-center'),
+            html.Span(text)
+        ], href=href, className='btn btn-secondary d-flex align-items-center justify-content-center btn-upgrade-pro')
     ], className=f'nav-item {active}')
 
 
@@ -86,18 +113,14 @@ def sideBar():
                 html.Div([
                     html.Div([
                         html.A([
-                            Svg([
-                                Path(strokeLinecap='round', strokeLinejoin='round', strokeWidth='2', d='M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1')
-                            ], className='icon icon-xxs me-1', fill='none', stroke='currentColor', viewBox='0 0 24 24', xmlns='http://www.w3.org/2000/svg'),
+                            signoutIcon(),
                             "Sign Out"
                         ], href='../pages/examples/sign-in.html', className='btn btn-secondary btn-sm d-inline-flex align-items-center')
                     ], className='d-block')
                 ], className='d-flex align-items-center'),
                 html.Div([
                     html.A([
-                        Svg([
-                            Path(fillRule='evenodd', d='M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z', clipRule='evenodd')
-                        ], className='icon icon-xs', fill='currentColor', viewBox='0 0 20 20', xmlns='http://www.w3.org/2000/svg')
+                        crossIcon()
                     ], href='#sidebarMenu', **{"data-bs-toggle": "collapse", "data-bs-target": "#sidebarMenu", "aria-controls": "sidebarMenu", "aria-expanded": "true", "aria-label": "Toggle navigation"})
                 ], className='collapse-close d-md-none')
             ], className='user-card d-flex d-md-none align-items-center justify-content-between justify-content-md-center pb-4'),
@@ -124,19 +147,8 @@ def sideBar():
 
                 # Bottom Item
 
-                html.Li([
-                    html.A([
-                        html.Span([
-                            Svg([
-                                Path(fillRule='evenodd', d='M12.395 2.553a1 1 0 00-1.45-.385c-.345.23-.614.558-.822.88-.214.33-.403.713-.57 1.116-.334.804-.614 1.768-.84 2.734a31.365 31.365 0 00-.613 3.58 2.64 2.64 0 01-.945-1.067c-.328-.68-.398-1.534-.398-2.654A1 1 0 005.05 6.05 6.981 6.981 0 003 11a7 7 0 1011.95-4.95c-.592-.591-.98-.985-1.348-1.467-.363-.476-.724-1.063-1.207-2.03zM12.12 15.12A3 3 0 017 13s.879.5 2.5.5c0-1 .5-4 1.25-4.5.5 1 .786 1.293 1.371 1.879A2.99 2.99 0 0113 13a2.99 2.99 0 01-.879 2.121z', clipRule='evenodd')
-                            ], className='icon icon-xs me-2', fill='currentColor', viewBox='0 0 20 20', xmlns='http://www.w3.org/2000/svg')
-                        ], className='sidebar-icon d-inline-flex align-items-center justify-content-center'),
-                        html.Span("Upgrade to Pro")
-                    ], href='../pages/upgrade-to-pro.html', className='btn btn-secondary d-flex align-items-center justify-content-center btn-upgrade-pro')
-                ], className='nav-item')
+                sidebarButtonLink("Upgrade to Pro", fireIcon, '../pages/upgrade-to-pro.html')
 
             ], className='nav flex-column pt-3 pt-md-0')
-
-
         ], className='sidebar-inner px-4 pt-3')
     ], id='sidebarMenu', className='sidebar d-lg-block bg-gray-800 text-white collapse', **{"data-simplebar": ""})
