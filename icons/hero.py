acf87cae89e8654b0b1f9889ddeb30e7cef0a396
Steve Jones <jonesst2608@gmail.com>
2022-01-20 18:02:28 +0000

    Bug fixing

--- icons/hero.py
+++ icons/hero.py
@@ -3,6 +3,10 @@ from dash_svg import Svg, Path
 
 # heroicons: https://heroicons.dev/
 
+ARROW_ICON = Svg([
+        Path(fillRule='evenodd', d='M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z', clipRule='evenodd')
+    ], className='icon icon-sm', fill='currentColor', viewBox='0 0 20 20', xmlns='http://www.w3.org/2000/svg')
+
 ALL_REPORTS_ICON = Svg([
         Path(d='M9 2a1 1 0 000 2h2a1 1 0 100-2H9z'),
         Path(fillRule='evenodd', d='M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm3 4a1 1 0 000 2h.01a1 1 0 100-2H7zm3 0a1 1 0 000 2h3a1 1 0 100-2h-3zm-3 4a1 1 0 100 2h.01a1 1 0 100-2H7zm3 0a1 1 0 100 2h3a1 1 0 100-2h-3z', clipRule='evenodd')
--- pages/components/sidebar.py
+++ pages/components/sidebar.py
@@ -1,9 +1,9 @@
 from dash import html, dcc
-from icons.hero import DOWN_ARROW_ICON, LIGHTENING_ICON, CHART_PIE_ICON, VIEW_GRID_ICON, CALENDER_ICON, TABLE_ICON, FIRE_ICON
+from icons.hero import LIGHTENING_ICON, CHART_PIE_ICON, VIEW_GRID_ICON, CALENDER_ICON, ARROW_ICON, TABLE_ICON, FIRE_ICON
 
 from .mobile_nav import mobileSidebarHeader
 
-def sidebarLink(text, icon, href, active=""):
+def _sidebarLink(text, icon, href, active=""):
     return  html.Li([
         dcc.Link([
             html.Span([
@@ -14,7 +14,7 @@ def sidebarLink(text, icon, href, active=""):
     ], className=f'nav-item {active}')
 
 
-def sidebarButtonLink(text, icon, href, active=""):
+def _sidebarButtonLink(text, icon, href, active=""):
     return  html.Li([
         html.A([
             html.Span([
@@ -25,7 +25,7 @@ def sidebarButtonLink(text, icon, href, active=""):
     ], className=f'nav-item {active}')
 
 
-def sidebarDropdown(text, icon, children):
+def _sidebarDropdown(text, icon, children):
     return html.Li([
 
         # Dropdown button
@@ -38,7 +38,7 @@ def sidebarDropdown(text, icon, children):
                 html.Span(text, className='sidebar-text')
             ]),
             html.Span([
-                DOWN_ARROW_ICON
+                ARROW_ICON
             ], className='link-arrow')
         ], className='nav-link collapsed d-flex justify-content-between align-items-center', **{"data-bs-toggle": "collapse", "data-bs-target": "#submenu-pages"}),
 
@@ -50,7 +50,7 @@ def sidebarDropdown(text, icon, children):
     ], className='nav-item')
 
 
-def dropdownEntry(text, href):
+def _dropdownEntry(text, href):
     return html.Li([
         html.A([
             html.Span(text, className='sidebar-text')
@@ -64,29 +64,29 @@ def sideBar():
 
             mobileSidebarHeader(),
 
-         # Sidebar List of entries
+            # Sidebar List of entries
 
             html.Ul([
-                sidebarLink("Volt Overview", LIGHTENING_ICON, 'https://demo.themesberg.com/volt/pages/dashboard/dashboard.html'),
-                sidebarLink("Dashboard", CHART_PIE_ICON, '/dashboard'),
-                sidebarLink("Settings", VIEW_GRID_ICON, '/settings'),
-                sidebarLink("Calendar", CALENDER_ICON, 'https://demo.themesberg.com/volt-pro/pages/calendar.html'),
+                _sidebarLink("Volt Overview", LIGHTENING_ICON, 'https://demo.themesberg.com/volt/pages/dashboard/dashboard.html'),
+                _sidebarLink("Dashboard", CHART_PIE_ICON, '/dashboard'),
+                _sidebarLink("Settings", VIEW_GRID_ICON, '/settings'),
+                _sidebarLink("Calendar", CALENDER_ICON, 'https://demo.themesberg.com/volt-pro/pages/calendar.html'),
 
                 # Page examples drop down
 
-                sidebarDropdown("Page examples", TABLE_ICON, [
-                    dropdownEntry("Sign In", '../pages/examples/sign-in.html'),
-                    dropdownEntry("Sign Up", '../pages/examples/sign-up.html'),
-                    dropdownEntry("Forgot password", '../pages/examples/forgot-password.html'),
-                    dropdownEntry("Reset password", '../pages/examples/reset-password.html'),
-                    dropdownEntry("Lock", '../pages/examples/lock.html'),
-                    dropdownEntry("404 Not Found", '../pages/examples/404.html'),
-                    dropdownEntry("500 Not Found", '../pages/examples/500.html'),
+                _sidebarDropdown("Page examples", TABLE_ICON, [
+                    _dropdownEntry("Sign In", '../pages/examples/sign-in.html'),
+                    _dropdownEntry("Sign Up", '../pages/examples/sign-up.html'),
+                    _dropdownEntry("Forgot password", '../pages/examples/forgot-password.html'),
+                    _dropdownEntry("Reset password", '../pages/examples/reset-password.html'),
+                    _dropdownEntry("Lock", '../pages/examples/lock.html'),
+                    _dropdownEntry("404 Not Found", '../pages/examples/404.html'),
+                    _dropdownEntry("500 Not Found", '../pages/examples/500.html'),
                 ]),
 
                 # Bottom Item
 
-                sidebarButtonLink("Upgrade to Pro", FIRE_ICON, '../pages/upgrade-to-pro.html')
+                _sidebarButtonLink("Upgrade to Pro", FIRE_ICON, '../pages/upgrade-to-pro.html')
 
             ], className='nav flex-column pt-3 pt-md-0')
         ], className='sidebar-inner px-4 pt-3')
