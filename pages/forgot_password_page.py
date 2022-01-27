f912bb385bef84e0e3dde9e061a15af5b0203ef4
Steve Jones <jonesst2608@gmail.com>
2022-01-27 17:39:25 +0000

    forgot_password_page.py now displays

--- pages/forgot_password_page.py
+++ pages/forgot_password_page.py
@@ -1,4 +1,9 @@
-layout = html.Body([
+from dash import html, dcc, register_page
+from dash_svg import Svg, Path
+
+register_page(__name__, path="/pages/forgot-password.html", title="Dash/Flightdeck - Forgot password")
+
+layout = html.Div([
     # NOTICE: You can use the _analytics.html partial to include production code specific code & trackers
     html.Main([
         # Section
@@ -7,9 +12,9 @@ layout = html.Body([
                 html.Div([
                     html.P([
                         html.A([
-                            html.Svg([
-                                html.Path(fillRule='evenodd', d='M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z', clipRule='evenodd')
-                            ], className='icon icon-xs me-2', fill='currentColor', viewbox='0 0 20 20', xmlns='http://www.w3.org/2000/svg'),
+                            Svg([
+                                Path(fillRule='evenodd', d='M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z', clipRule='evenodd')
+                            ], className='icon icon-xs me-2', fill='currentColor', viewBox='0 0 20 20', xmlns='http://www.w3.org/2000/svg'),
                             "Back to log in"
                         ], href='./sign-in.html', className='d-flex align-items-center justify-content-center')
                     ], className='text-center'),
@@ -22,7 +27,7 @@ layout = html.Body([
                                 html.Div([
                                     html.Label("Your Email", htmlFor='email'),
                                     html.Div([
-                                        html.Input(type='email', className='form-control', id='email', placeholder='john@company.com', required='', autofocus='')
+                                        dcc.Input(type='email', className='form-control', id='email', placeholder='john@company.com', required='')
                                     ], className='input-group')
                                 ], className='mb-4'),
                                 # End of Form
@@ -35,33 +40,5 @@ layout = html.Body([
                 ], className='row justify-content-center form-bg-image')
             ], className='container')
         ], className='vh-lg-100 mt-5 mt-lg-0 bg-soft d-flex align-items-center')
-    ]),
-    # Core
-    html.Script(src='../../vendor/@popperjs/core/dist/umd/popper.min.js'),
-    html.Script(src='../../vendor/bootstrap/dist/js/bootstrap.min.js'),
-    # Vendor JS
-    html.Script(src='../../vendor/onscreen/dist/on-screen.umd.min.js'),
-    # Slider
-    html.Script(src='../../vendor/nouislider/distribute/nouislider.min.js'),
-    # Smooth scroll
-    html.Script(src='../../vendor/smooth-scroll/dist/smooth-scroll.polyfills.min.js'),
-    # Charts
-    html.Script(src='../../vendor/chartist/dist/chartist.min.js'),
-    html.Script(src='../../vendor/chartist-plugin-tooltips/dist/chartist-plugin-tooltip.min.js'),
-    # Datepicker
-    html.Script(src='../../vendor/vanillajs-datepicker/dist/js/datepicker.min.js'),
-    # Sweet Alerts 2
-    html.Script(src='../../vendor/sweetalert2/dist/sweetalert2.all.min.js'),
-    # Moment JS
-    html.Script(src='https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.27.0/moment.min.js'),
-    # Vanilla JS Datepicker
-    html.Script(src='../../vendor/vanillajs-datepicker/dist/js/datepicker.min.js'),
-    # Notyf
-    html.Script(src='../../vendor/notyf/notyf.min.js'),
-    # Simplebar
-    html.Script(src='../../vendor/simplebar/dist/simplebar.min.js'),
-    # Github buttons
-    html.Script(async='', defer='', src='https://buttons.github.io/buttons.js'),
-    # Volt JS
-    html.Script(src='../../assets/js/volt.js')
+    ])
 ])
