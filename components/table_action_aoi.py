95bfc0a27e58d5994a59eb00f1f5b9086a089f99
Steve Jones <jonesst2608@gmail.com>
2022-02-08 18:55:54 +0000

    TableActionAIO now working with my Matcher factory

--- components/table_action_aoi.py
+++ components/table_action_aoi.py
@@ -1,9 +1,6 @@
-import uuid
-import logging
-from dash import Output, Input, State, html, dcc, callback, MATCH
+from dash import html, dcc, callback, MATCH
 import dash_holoniq_components as dhc
-
-from icons.hero import ARROW_ICON
+from dash_spa import match, component_uuid
 
 def dropdownFolderEntry(text, href):
     return html.Li([
@@ -15,18 +12,27 @@ def dropdownFolderEntry(text, href):
 class TableActionAIO(html.Div):
 
     class ids:
-        button = lambda aio_id: {
-            'component': 'TableActionAIO',
-            'subcomponent': 'button',
-            'aio_id': aio_id
-        }
-        container = lambda aio_id: {
-            'component': 'TableActionAIO',
-            'subcomponent': 'container',
-            'aio_id': aio_id
-        }
+        button = match({'component': 'TableActionAIO', 'subcomponent': 'button', 'idx': MATCH})
+        container = match({'component': 'TableActionAIO', 'subcomponent': 'container', 'idx': MATCH})
 
-    ids = ids
+    # pylint: disable=no-self-argument
+
+    @callback(ids.container.output.className, ids.button.input.n_clicks, ids.button.input.focus, ids.container.state.className)
+    def show_dropdown(button_clicks, button_focus, className):
+
+        if not button_clicks:
+            return className
+
+        classNames = className.split()
+
+        if 'show' in classNames:
+            if button_focus is False:
+                classNames.remove('show')
+        else:
+            classNames.append('show')
+
+        return ' '.join(classNames)
+
 
     def __init__(self, children, aio_id=None):
         """Table Action component, shows children in dropdown
@@ -36,40 +42,36 @@ class TableActionAIO(html.Div):
             aio_id (str, optional): The component ID. Defaults to None.
         """
 
-        if aio_id is None:
-            aio_id = str(uuid.uuid4())
+        ids = TableActionAIO.ids
+        aio_id = aio_id if aio_id else component_uuid()
 
-        _button = dhc.Button([
+        button = dhc.Button([
                 html.Span([
                     html.Span(className='fas fa-ellipsis-h icon-dark')
                 ], className='icon icon-sm'),
                 html.Span("Toggle Dropdown", className='visually-hidden')
-            ], id=self.ids.button(aio_id), className='btn btn-link text-dark dropdown-toggle dropdown-toggle-split m-0 p-0')
+            ], id=ids.button.idx(aio_id), className='btn btn-link text-dark dropdown-toggle dropdown-toggle-split m-0 p-0')
 
         # Drop down container
 
-        _container = html.Div(children, id=self.ids.container(aio_id), className='dropdown-menu py-0')
+        # dropdown bootom-left. Ripped from the Volt transactons table using Firefox debug tools
+
+        style={"position": "absolute",
+               "inset": "0px 0px auto auto",
+               "margin": "0px",
+               "transform": "translate3d(0px, 25.3333px, 0px)"
+               }
+
+        container = html.Div(
+            children,
+            id = ids.container.idx(aio_id),
+            className='dropdown-menu py-0', style=style)
 
         super().__init__([
             html.Div([
-                _button,
-                _container
-            ], className='btn-group')
+                button,
+                container
+            ], className='btn-group', style={"float" : "left"})
         ])
-
-    @callback(Output(ids.container(MATCH), 'className'),
-            Input(ids.button(MATCH), 'n_clicks'),
-            Input(ids.button(MATCH), 'focus'),
-            State(ids.container(MATCH), 'className'))
-    def show_dropdown(button_clicks, button_focus, className):
-        logging.info('show_dropdown: button_clicks=%s, className = %s', button_clicks, className)
-
-        if not button_clicks:
-            return className
-
-        if 'show' in className and button_focus == False:
-            return className.replace(' show', '')
-        else:
-            return className + ' show'
 
 
