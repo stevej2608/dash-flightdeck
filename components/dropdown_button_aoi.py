73f91591f5b1ae2a4c74cf715cb10320e7f1f366
Steve Jones <jonesst2608@gmail.com>
2022-02-09 09:55:43 +0000

    DropdownButtonAIO is now a subclass of DropdownAIO

--- components/dropdown_button_aoi.py
+++ components/dropdown_button_aoi.py
@@ -1,7 +1,6 @@
-from dash import html, dcc, callback, MATCH
-import dash_holoniq_components as dhc
-from dash_spa import match, component_uuid
+from dash import html, dcc
 from icons.hero import DOWN_ARROW_ICON, PLUS_ICON
+from .dropdown_aio import DropdownAIO
 
 def dropdownLink(title, icon, href='#'):
     return dcc.Link([
@@ -9,30 +8,7 @@ def dropdownLink(title, icon, href='#'):
         title
     ], className='dropdown-item d-flex align-items-center', href=href)
 
-class DropdownButtonAIO(html.Div):
-
-    class ids:
-        button = match({'component': 'DropdownButtonAIO', 'subcomponent': 'button', 'idx': MATCH})
-        container = match({'component': 'DropdownButtonAIO', 'subcomponent': 'container', 'idx': MATCH})
-
-    # pylint: disable=no-self-argument
-
-    @callback(ids.container.output.className, ids.button.input.n_clicks, ids.button.input.focus, ids.container.state.className)
-    def show_dropdown(button_clicks, button_focus, className):
-
-        if not button_clicks:
-            return className
-
-        classNames = className.split()
-
-        if 'show' in classNames:
-            if button_focus is False:
-                classNames.remove('show')
-        else:
-            classNames.append('show')
-
-        return ' '.join(classNames)
-
+class DropdownButtonAIO(DropdownAIO):
 
     def __init__(self, dropdownEntries, buttonText, buttonIcon=PLUS_ICON, buttonColor='secondary', downArrow=False, aio_id=None):
         """Button with supplied icon and down arrow. When clicked a drop-down
@@ -62,20 +38,16 @@ class DropdownButtonAIO(html.Div):
 
         """
 
-        ids = DropdownButtonAIO.ids
-        aio_id = aio_id if aio_id else component_uuid()
-
-        button = dhc.Button([
+        button = DropdownAIO.Button([
                 buttonIcon,
                 buttonText,
                 DOWN_ARROW_ICON if downArrow else None
-            ], id=ids.button.idx(aio_id), className=f'btn btn-{buttonColor} d-inline-flex align-items-center me-2 dropdown-toggle')
+            ], className=f'btn btn-{buttonColor} d-inline-flex align-items-center me-2 dropdown-toggle')
 
         # Drop down container
 
         container = html.Div(
             dropdownEntries,
-            id = ids.container.idx(aio_id),
             className='dropdown-menu dashboard-dropdown dropdown-menu-start mt-2 py-1')
 
-        super().__init__(html.Div([button, container], className='dropdown'))
+        super().__init__(button, container)
