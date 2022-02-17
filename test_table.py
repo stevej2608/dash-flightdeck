1546d4514ee6d62c771b2806dd4255cff8cf8ff6
Steve Jones <jonesst2608@gmail.com>
2022-02-17 15:50:41 +0000

    Skeleton search in place

--- test_table.py
+++ test_table.py
@@ -16,6 +16,67 @@ from components.table import TableAIOPaginator, TableAIOPaginatorView
 
 from icons.hero import TICK_ICON, GEAR_ICON, SEARCH_ICON
 
+_operators = [
+    ['ge ', '>='],
+    ['le ', '<='],
+    ['lt ', '<'],
+    ['gt ', '>'],
+    ['ne ', '!='],
+    ['eq ', '='],
+    ['contains '],
+    ['datestartswith ']]
+
+def _split_filter_part(filter_part):
+    for operator_type in _operators:
+        for operator in operator_type:
+            if operator in filter_part:
+                name_part, value_part = filter_part.split(operator, 1)
+                name = name_part[name_part.find('{') + 1: name_part.rfind('}')]
+
+                value_part = value_part.strip()
+                v0 = value_part[0]
+                if (v0 == value_part[-1] and v0 in ("'", '"', '`')):
+                    value = value_part[1: -1].replace('\\' + v0, v0)
+                else:
+                    try:
+                        value = float(value_part)
+                    except ValueError:
+                        value = value_part
+                # word _operators need spaces after them in the filter string,
+                # but we don't want these later
+                return name, operator_type[0].strip(), value
+
+    return [None] * 3
+
+def filter_df(df, filter_query):
+    """Filter a Pandas dataframe as per the `filter_query` provided by
+    the DataTable.
+
+    https://dash.plotly.com/datatable/callbacks#backend-paging-with-filtering
+
+    """
+
+    try:
+
+        filtering_expressions = filter_query.split(' && ')
+        for filter_part in filtering_expressions:
+            col_name, operator, filter_value = _split_filter_part(filter_part)
+
+            if operator in ('eq', 'ne', 'lt', 'le', 'gt', 'ge'):
+                # these _operators match pandas series operator method names
+                df = df.loc[getattr(df[col_name], operator)(filter_value)]
+            elif operator == 'contains':
+                df = df.loc[df[col_name].str.contains(filter_value)]
+            elif operator == 'datestartswith':
+                # this is a simplification of the front-end filtering logic,
+                # only works with complete fields in standard format
+                df = df.loc[df[col_name].str.startswith(filter_value)]
+
+    except:
+        pass
+
+    return df
+
 
 class SearchAIO(html.Div):
 
@@ -26,10 +87,11 @@ class SearchAIO(html.Div):
 
         search = dcc.Input(id=pid('input'), type='text', className='form-control', placeholder=placeholder)
 
-        @callback(self.store.output.data, search.input.value)
-        def _cb(value=""):
+        @callback(self.store.output.data, search.input.value, self.store.state.data)
+        def _cb(value, store):
             log.info('value = [%s]', value)
-            return value
+            store['search'] = value
+            return store
 
 
         ui = html.Div([
@@ -122,8 +184,9 @@ class TableRow(html.Tr):
         ])
 
 class TableBody(html.Tbody):
-    def __init__(self, rows):
-        super().__init__([TableRow(*args) for args in rows])
+    def __init__(self, rows, id=None):
+        pid = prefix(id)
+        super().__init__([TableRow(*args) for args in rows], id=pid('tbody'))
 
 class TableHead(html.Thead):
     def __init__(self, columns):
@@ -131,12 +194,38 @@ class TableHead(html.Thead):
         super().__init__(row)
 
 class Table(html.Table):
-    def __init__(self, df, thead, tbody, className=None):
+    def __init__(self, df: pd.DataFrame, thead: TableHead, tbody: TableBody, id=None, search:dcc.Store = None, className=None):
+        self.df = df
+        pid = prefix(id)
+
         thead = thead(df.columns)
-        tbody = tbody(df[0:5].values.tolist())
+        tbody = tbody(df[0:5].values.tolist(), id=pid())
+
+        # https://stackoverflow.com/questions/26640129/search-for-string-in-all-pandas-dataframe-columns-and-filter
+        # ID eq 45697
+
+        if search is not None:
+            @callback(tbody.output.children, search.input.data)
+            def _cb(value):
+
+                log.info('value = [%s]', value)
+
+                if value is None:
+                    raise PreventUpdate
+
+                value = value['search']
+
+                if value is None:
+                    raise PreventUpdate
+
+                df = filter_df(self.df, value)
+                rows = df[0:10].values.tolist()
+                return [TableRow(*args) for args in rows]
+
+
         super().__init__([thead, tbody], className=className)
 
-def table(df):
+def table(df, search: dcc.Store):
 
     paginator = TablePaginator(
         ["Previous", 1, 2, 3, 4, 5, "Next"], 1, 25,
@@ -144,7 +233,7 @@ def table(df):
         )
 
     return html.Div([
-        Table(df, TableHead, TableBody, className='table table-hover'),
+        Table(df, TableHead, TableBody, search=search, className='table table-hover'),
         paginator
     ], className='card card-body border-0 shadow table-wrapper table-responsive')
 
@@ -169,7 +258,7 @@ def layout():
         html.Main([
             html.Div(className='d-flex py-4'),
             pageHeader(search, settingsDropdown),
-            table(df),
+            table(df, search.store),
         ], className='content')
     ])
 
