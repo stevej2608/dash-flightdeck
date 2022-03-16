c17f5d524571ec07f0c1c0c9f6e3a252ee86f290
Steve Jones <jonesst2608@gmail.com>
2022-03-16 07:45:36 +0000

    Converted DataTable to class with over loadable methods for head, body, row, etc

--- dash_spa/spa_table/DataTable.py
+++ dash_spa/spa_table/DataTable.py
@@ -1,53 +1,21 @@
 from typing import List, Dict, Any
 from dash import html
-from dash.development.base_component import Component
 
 from dash_spa import prefix
+
+TableData = List[Dict[str, Any]]
 
 TableColumns = List[Dict[str, Any]]
 
 CELL_STYLE={"border-color": "rgb(211, 211, 211)", "border-style": "solid", "border-width": "1px"}
 
-class TableRow(html.Tr):
-    def __init__(self, Date, Region, Temperature, Humidity, Pressure):
-        super().__init__([
-            html.Td(html.A(Date, href='#'), className="dash-cell column-0", style=CELL_STYLE),
-            html.Td(html.Span(Region), className="dash-cell column-0",style=CELL_STYLE),
-            html.Td(html.Span(Temperature), className="dash-cell column-0", style=CELL_STYLE),
-            html.Td(html.Span(Humidity), className="dash-cell column-0", style=CELL_STYLE),
-            html.Td(html.Span(Pressure), className="dash-cell column-0", style=CELL_STYLE)
-        ])
-
-class TableBody(html.Tbody):
-    def __init__(self, rows, id=None):
-        pid = prefix(id)
-        super().__init__([TableRow(**args) for args in rows], id=pid('tbody'))
-
-class TableHead(html.Thead):
-    def __init__(self, columns: TableColumns):
-        row =  html.Tr([html.Th(col['name'], className="dash-header column-0 ", style=CELL_STYLE) for col in columns])
-        super().__init__(row)
-
-
-class Table(html.Table):
-    def __init__(self, data: Dict, columns: TableColumns, thead: TableHead, tbody: TableBody, id=None, className='cell-table'):
-        pid = prefix(id)
-
-        thead = thead(columns)
-        tbody = tbody(data, id=pid())
-
-        super().__init__([thead, tbody], className=className)
-
-
 class DataTable(html.Div):
 
-    def __init__(
-        self,
-        data=Component.UNDEFINED,
-        columns: TableColumns = Component.UNDEFINED,
-        page_size: int = Component.UNDEFINED
-    ):
-        table = Table(data[0:page_size], columns, TableHead, TableBody)
+    def __init__(self, data: TableData, columns: TableColumns, page_size: int = None, id: str = None):
+
+        self.pid = prefix(id)
+
+        table = self.table(data[0:page_size], columns)
 
         spreadsheet_inner = html.Div(table,
             className="dash-spreadsheet-inner dash-spreadsheet dash-empty-01 dash-no-filter dash-fill-width",
@@ -65,3 +33,25 @@ class DataTable(html.Div):
             )
 
         super().__init__(table_container)
+
+    def table(self, data: TableData, columns: TableColumns, id=None, className='cell-table'):
+        pid = prefix(id)
+        thead = self.tableHead(columns)
+        tbody = self.tableBody(data)
+        return html.Table([thead, tbody], className=className)
+
+    def tableHead(self, columns: TableColumns):
+        row =  html.Tr([html.Th(col['name'], className="dash-header column-0 ", style=CELL_STYLE) for col in columns])
+        return html.Thead(row)
+
+    def tableBody(self, rows):
+        return html.Tbody([self.tableRow(**args) for args in rows], id=self.pid('tbody'))
+
+    def tableRow(self, Date, Region, Temperature, Humidity, Pressure):
+        return html.Tr([
+            html.Td(html.A(Date, href='#'), className="dash-cell column-0", style=CELL_STYLE),
+            html.Td(html.Span(Region), className="dash-cell column-0",style=CELL_STYLE),
+            html.Td(html.Span(Temperature), className="dash-cell column-0", style=CELL_STYLE),
+            html.Td(html.Span(Humidity), className="dash-cell column-0", style=CELL_STYLE),
+            html.Td(html.Span(Pressure), className="dash-cell column-0", style=CELL_STYLE)
+        ])
